0744dd172b522ac9088828c126264e30e3e009b8
Tiago Fernandes <tiago@zavamed.com>
2019-09-08 18:37:03 +0100

    update default.js

--- config/default.js
+++ config/default.js
@@ -1,29 +1,26 @@
 const routes = require('./routes.json')
 
 module.exports = {
-  allowedLanguages: ['es'],
-  allowed_languages : ['es'],
+  allowedLanguages: ['es', 'fr'],
   google: {
-    ga:  process.env.ga || 'UA-XXXXXXXX-1'
+    ga: process.env.ga || 'UA-XXXXXXXX-1'
   },
   tumblr: {
-    on: process.env.tumblr_on ? process.env.tumblr_on === "true" : true,
+    on: process.env.tumblr_on ? process.env.tumblr_on === 'true' : true,
     oauth: {
       consumer_key :  process.env.tumblr_key,
       consumer_secret :  process.env.tumblr_secret,
-      token: process.env.tumblr_token,
-      token_secret: process.env.tumblr_token_secret
     },
     blog: process.env.tumblr_blog || 'babelpde.tumblr.com'
   },
   news: {
     limit: 3,
-    templates: {'en':'news','es':'noticias','cat':'noticies'},
-    templates_front_page: {'en':'news_fp','es':'home','cat':'noticies_fp'}
+    templates: {'en': 'news','es': 'noticias','cat': 'noticies'},
+    templates_front_page: {'en': 'news_fp','es': 'home','fr': 'home'}
   },
-  is_mi_grano_de_arena: process.env.mi_grano_de_arena ? process.env.mi_grano_de_arena === "true" : false,
+  is_mi_grano_de_arena: process.env.mi_grano_de_arena ? process.env.mi_grano_de_arena === 'true' : false,
   routes,
   layout: 'babel'
-};
+}
 
 module.exports.routes = routes
